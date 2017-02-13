cce573652bbc9c450ab54994b424e635feaebf4c
Sebastian Duque Restrepo <sebas_tian_95@hotmail.com>
2017-02-12 22:58:35 -0500

    add phaserjs, score and ball

--- game/www/js/game.js
+++ game/www/js/game.js
@@ -1,15 +1,71 @@
 const app = {
   init() {
-    navigator.accelerometer.watchAcceleration(this.onSuccess, this.onError, {frequency: 1000});
+    BALLDIAMETER = 50;
+
+    speedX = 0;
+    speedY = 0;
+    score = 0;
+
+    HEIGHT = document.documentElement.clientHeight;
+    WIDTH = document.documentElement.clientWidth;
+
+    app.sensorsMonitor();
+    app.startGame();
   },
 
-  onError() {
-    console.log('Error');
+  startGame() {
+    function preload() {
+      game.physics.startSystem(Phaser.Physics.ARCADE);
+      game.stage.backgroundColor = '#f27d0c';
+      game.load.image('ball', 'img/ball.png');
+    }
+
+    function create() {
+      scoreText = game.add.text(16, 16, score, {fontSize: '100px', fill: '#757676'})
+      ball = game.add.sprite(app.initX(), app.initY(), 'ball');
+      game.physics.arcade.enable(ball);
+      ball.body.collideWorldBounds = true;
+      ball.body.onWorldBounds = new Phaser.Signal();
+      ball.body.onWorldBounds.add(app.decrementScore, this);
+    }
+
+    function update() {
+      ball.body.velocity.y = (speedY * 300);
+      ball.body.velocity.x = (speedX * -300);
+    }
+
+    const state = {preload, create, update};
+    const game = new Phaser.Game(WIDTH, HEIGHT, Phaser.CANVAS, 'phaser', state);
   },
 
-  onSuccess(data) {
-    app.detectAgitation(data);
-    app.representValues(data);
+  decrementScore() {
+    score--;
+    scoreText.text = score;
+  },
+
+  initX() {
+    return app.randomNumberTo(WIDTH - BALLDIAMETER);
+  },
+
+  initY() {
+    return app.randomNumberTo(HEIGHT - BALLDIAMETER);
+  },
+
+  randomNumberTo(limit) {
+    return Math.floor(Math.random() * limit);
+  },
+
+  sensorsMonitor() {
+    function onError() {
+      console.log('onError');
+    }
+
+    function onSuccess(data) {
+      app.detectAgitation(data);
+      app.addressRegister(data);
+    }
+
+    navigator.accelerometer.watchAcceleration(onSuccess, onError, {frequency: 10});
   },
 
   detectAgitation(data) {
@@ -17,21 +73,17 @@ const app = {
     const agitationY = data.y > 10;
 
     if (agitationX || agitationY) {
-      document.body.className = 'agitado';
-    } else {
-      document.body.className = '';
+      setTimeout(app.restart, 1000);
     }
   },
 
-  representValues(data) {
-    app.represent(data.x, '#valorx');
-    app.represent(data.y, '#valory');
-    app.represent(data.z, '#valorz');
+  restart() {
+    document.location.reload(true);
   },
 
-  represent(data, HTMLElement) {
-    const round = Math.round(data * 100) / 100;
-    document.querySelector(HTMLElement).innerHTML = round;
+  addressRegister(data) {
+    speedX = data.x;
+    speedY = data.y;
   }
 };
 
